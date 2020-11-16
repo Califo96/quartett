ff585798fadcf95e5b06efeaa05b9d4c4259ec99
Moritz Schruff <526632@fom-net.de>
2020-11-16 14:23:29 +0100

    Erstelen des Projekts

---
+++ Quartett/Quartett/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Quartett
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
