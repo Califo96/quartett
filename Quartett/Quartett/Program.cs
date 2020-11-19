964bfea0812723d792b32dcd4e836e4d1d22d43e
Exoitus <vinzent.joest@outlook.de>
2020-11-19 11:31:34 +0100

    Änderung Console WriteLine

--- Quartett/Quartett/Program.cs
+++ Quartett/Quartett/Program.cs
@@ -6,7 +6,7 @@ namespace Quartett
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Basti stinkt!");
         }
     }
 }
