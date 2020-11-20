54abe1bbc07dad98ad3d68c2e387a82a513425a4
Exoitus <vinzent.joest@outlook.de>
2020-11-20 14:40:55 +0100

    no message

--- Quartett/Quartett/Program.cs
+++ Quartett/Quartett/Program.cs
@@ -7,16 +7,52 @@ namespace Quartett
     class Controller
     {
         private int Spieler;
-        
+
+        private void go()
+        {
+            Console.WriteLine("Bevor ihr los legen könnt, sagt uns schnell wie viele Spieler ihr seid!");
+            Console.Write("Anzahl Spieler ( 2 - 6 ): ");
+            int m = int.Parse(Console.ReadKey().KeyChar.ToString());
+
+            if (m > 6 || m < 2)
+            {
+                Console.Clear();
+                Console.WriteLine("Die Anzahl der angegebenen Spieler liegt außerhalb der erlaubten Spielerzahl oder die Eingabe war nicht numerisch!");
+            }
+            Spieler = m;
+        }
+
+        private void regeln()
+        {
+            Console.WriteLine("blablablabla");
+            Console.WriteLine("Willst du zurück? dann drücke Z");
+            char b = Console.ReadKey().KeyChar;
+            if (b.Equals('z'))
+            {
+                startmenu();
+            }
+            else if (b.Equals('s'))
+            {
+                go();
+            }
+        }
         private void startmenu()
         {
-            
+
             Console.WriteLine("Hallo! Willkommen beim Quartettspiel!");
-            Console.WriteLine("Wenn ihr loslegen wollt, sagt uns schnell wie viele Spieler ihr seid!");
-            Console.Write("Anzahl Spieler ( > 2 ): ");
-            int m = int.Parse(Console.ReadKey().KeyChar.ToString());
-            Spieler = m;
-            Console.Clear();
+            Console.WriteLine("Willst du starten? Dann drücke S");
+            Console.WriteLine("Willst du die Regeln nochmal lesen? Dann drücke R");
+            char a = Console.ReadKey().KeyChar;
+            if (a.Equals('s'){
+                go();
+            }
+            else if (a.Equals('r'))
+            {
+                regeln();
+            }
+            
+            //Console.WriteLine(m);
+            //Console.Clear();
         }
 
         static void Main(string[] args)
