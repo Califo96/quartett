506c7b02b196026bff1e41f00a85c9790343ef5f
Moritz Schruff <moritz.schruff@gmx.de>
2020-11-20 12:50:39 +0100

    Moritz Änderung

--- Quartett/Quartett/Spiel.cs
+++ Quartett/Quartett/Spiel.cs
@@ -15,6 +15,8 @@ namespace Quartett
         {
             anzahlSpieler = anzahl;
             kartenSchlangen = new Queue<Karte>[anzahlSpieler];
+
+
         }
 
         private void austeilen()
@@ -27,10 +29,103 @@ namespace Quartett
 
         }
 
-        private int vergleiche()
+        private int vergleiche(int vergleichsWert)
         {
-            return 0;
+            int besterWert=0;
+            double besterWertD = 0;
+            int indexVonBestemWert = 0;
+            char vergleichsoperator;
+            String vergleich;
+            bool gleicherWertVorhanden=false;
+
+
+            for (int i = 0; i < anzahlSpieler-1; i++)
+            {
+                switch (vergleichsWert)
+                {
+                        case 0:                                                        //Fall PS Zahl vergleichen
+                        if (kartenSchlangen[i].Dequeue.getPs()>besterWert)
+                        {
+                            besterWert = kartenSchlangen[i].Dequeue.getPs();
+                            indexVonBestemWert = i;
+                        }else if (kartenSchlangen[i].Dequeue.getPs() == besterWert)
+                        {
+                            gleicherWertVorhanden = true;
+                            tempStapel.Push(kartenSchlangen[i].Dequeue);
+                            tempStapel.Push(kartenSchlangen[besterWert].Dequeue);
+                        }
+                        break;
+
+                    case 1:
+                        if (kartenSchlangen[i].Dequeue.getBeschleunigung() < besterWertD)
+                        {
+                            besterWertD = kartenSchlangen[i].Dequeue.getBeschleunigung();
+                            indexVonBestemWert = i;
+                        }
+                        else if (kartenSchlangen[i].Dequeue.getBeschleunigung() == besterWertD)
+                        {
+                            gleicherWertVorhanden = true;
+                            tempStapel.Push(kartenSchlangen[i].Dequeue);
+                            tempStapel.Push(kartenSchlangen[besterWert].Dequeue);
+                        }
+                        break;
+                    case 2:
+                        if (kartenSchlangen[i].Dequeue.getMaxV() > besterWert)
+                        {
+                            besterWert = kartenSchlangen[i].Dequeue.getMaxV();
+                            indexVonBestemWert = i;
+                        }
+                        else if (kartenSchlangen[i].Dequeue.getMaxV() == besterWert)
+                        {
+                            gleicherWertVorhanden = true;
+                            tempStapel.Push(kartenSchlangen[i].Dequeue);
+                            tempStapel.Push(kartenSchlangen[besterWert].Dequeue);
+                        }
+                        break;
+                    case 3:
+                        if (kartenSchlangen[i].Dequeue.getGewicht() < besterWert)
+                        {
+                            besterWert = kartenSchlangen[i].Dequeue.getGewicht();
+                            indexVonBestemWert = i;
+                        }
+                        else if (kartenSchlangen[i].Dequeue.getGewicht() == besterWert)
+                        {
+                            gleicherWertVorhanden = true;
+                            tempStapel.Push(kartenSchlangen[i].Dequeue);
+                            tempStapel.Push(kartenSchlangen[besterWert].Dequeue);
+                        }
+                        break;
+                    case 4:
+                        if (kartenSchlangen[i].Dequeue.getHubraum() > besterWert)
+                        {
+                            besterWert = kartenSchlangen[i].Dequeue.Hubraum();
+                            indexVonBestemWert = i;
+                        }
+                        else if (kartenSchlangen[i].Dequeue.getHubraum() == besterWert)
+                        {
+                            gleicherWertVorhanden = true;
+                            tempStapel.Push(kartenSchlangen[i].Dequeue);
+                            tempStapel.Push(kartenSchlangen[besterWert].Dequeue);
+                        }
+                        break;
+                    case 5:
+                        if (kartenSchlangen[i].Dequeue.getZylinder() > besterWert)
+                        {
+                            besterWert = kartenSchlangen[i].Dequeue.getZylinder();
+                            indexVonBestemWert = i;
+                        }
+                        else if (kartenSchlangen[i].Dequeue.getZylinder() == besterWert)
+                        {
+                            gleicherWertVorhanden = true;
+                            tempStapel.Push(kartenSchlangen[i].Dequeue);
+                            tempStapel.Push(kartenSchlangen[besterWert].Dequeue);
+                        }
+                        break;
+                }
+            }
         }
+
+     }
 
         private void printcard()
         {
