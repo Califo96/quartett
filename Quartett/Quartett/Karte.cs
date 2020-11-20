3e443a6711028d519985ea21c4f58494b6ecd956
Sebastian <dkpinguin07@gmail.com>
2020-11-20 11:37:42 +0100

    Karte cs hinzufügen

--- Quartett/Quartett/Karte.cs
+++ Quartett/Quartett/Karte.cs
@@ -4,7 +4,28 @@ using System.Text;
 
 namespace Quartett
 {
-    class Class1
+    class Karte
     {
+        private int ps;
+        private double beschleunigung;
+        private int maxV;
+        private int gewicht;
+        private int hubraum;
+        private int zylinder;
+        private int index;
+        private string name;
+        private string herLand;
+
+        public Karte(int pps, double pbeschleunigung, int pmaxV, int pgewicht, int phubraum, int pzylinder, string pname, string pherLand)
+        {
+            ps = pps;
+            beschleunigung = pbeschleunigung;
+            maxV = pmaxV;
+            gewicht = pgewicht;
+            hubraum = phubraum;
+            zylinder = pzylinder;
+            name = pname;
+            herLand = pherLand;
+        }
     }
 }
